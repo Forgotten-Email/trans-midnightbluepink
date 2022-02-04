67c3caff48b0eb0f6175f13817e3f2ba127f4f11
Forgotten-Email <96489361+Forgotten-Email@users.noreply.github.com>
2022-02-04 23:05:41 +0000

    Update config.fish
    Add ncmpcpp-ueberzug alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -12,7 +12,8 @@ if status is-interactive
     alias matrix='unimatrix -f -l ocCgGkS -s 96'
     alias clock='tty-clock -sct -C 4'
     alias pipes='pipes -p 5 -R -t 1 -r 0'
-    alias ncmpcpp-tmux='tmux new-session -s $fish_pid "tmux source-file ~/.config/ncmpcpp/tsession"'       
-    colorscript -r
+    alias ncmpcpp-tmux='tmux new-session -s $fish_pid "tmux source-file $HOME/.config/ncmpcpp/tsession"'       
+    alias ncmpcpp-ueberzug="$HOME/.config/ncmpcpp/ncmpcpp-ueberzug/ncmpcpp-ueberzug"
+   colorscript -r
 end
 
