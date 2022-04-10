446e45d5b059e5a0de5efe8b8f9be9986cb04a1a
Forgotten-Email <96489361+Forgotten-Email@users.noreply.github.com>
2022-04-10 20:48:58 +0100

    Update Neovim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,12 +1,31 @@
-local chad_modules = {
-   "options",
-   "mappings",
+local present, impatient = pcall(require, "impatient")
+
+if present then
+   impatient.enable_profile()
+end
+
+local core_modules = {
+   "core.options",
+   "core.autocmds",
+   "core.mappings",
 }
 
-for i = 1, #chad_modules, 1 do
-   if not pcall(require, chad_modules[i]) then
-      error("Error loading " .. chad_modules[i] .. "\n")
+for _, module in ipairs(core_modules) do
+   local ok, err = pcall(require, module)
+   if not ok then
+      error("Error loading " .. module .. "\n\n" .. err)
    end
 end
 
-require("mappings").misc()
+-- non plugin mappings
+require("core.mappings").misc()
+
+-- check if custom init.lua file exists
+if vim.fn.filereadable(vim.fn.stdpath "config" .. "/lua/custom/init.lua") == 1 then
+   -- try to call custom init, if not successful, show error
+   local ok, err = pcall(require, "custom")
+   if not ok then
+      vim.notify("Error loading custom/init.lua\n\n" .. err)
+   end
+   return
+end
